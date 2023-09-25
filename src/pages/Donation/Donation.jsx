3aaf02c852e8ad025579016fcb9ed1729385132e
nayon117 <hasibulhasannayon260@gmail.com>
2023-09-25 15:30:13 +0600

    completed design and all functionality in donation page

--- src/pages/Donation/Donation.jsx
+++ src/pages/Donation/Donation.jsx
@@ -1,9 +1,47 @@
+import { useEffect, useState } from "react";
+import DonationCard from "./DonationCard";
+
  
 
 const Donation = () => {
+    const [donations, setDonations] = useState([])
+    const [noFound, setNoFound] = useState(false)
+    const [IsShow,setIsShow] = useState(false)
+    
+    useEffect(() => {
+        const donationsItems = JSON.parse(localStorage.getItem("donations"))
+        if (donationsItems) {
+            setDonations(donationsItems)
+        }
+        else {
+            setNoFound("No Data Found")
+        }
+    },[])
     return (
         <div>
-            donation
+            {
+                noFound ? <p className="h-[70vh] flex justify-center items-center text-2xl font-bold">{noFound}</p>
+                    :
+        <div>
+                <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 py-10">
+                {
+                        IsShow ? donations.map(category => <DonationCard
+                            key={category.id}
+                            category={category}
+                        ></DonationCard>) :   
+                        donations.slice(0,4).map(category => <DonationCard
+                            key={category.id}
+                            category={category}
+                        ></DonationCard>)      
+                }     
+                        </div>
+                        {
+                            donations.length > 4 && <button className="px-3 my-4 py-2 bg-red-500 rounded-md text-white block mx-auto" onClick={() => setIsShow(!IsShow)}>
+                                {IsShow ? 'See Less' : "See More"}
+                            </button>
+            }            
+            </div>
+             }
         </div>
     );
 };
---
+++ src/pages/Donation/DonationCard.jsx
@@ -0,0 +1,48 @@
+ 
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const DonationCard = ({ category }) => {
+    const {id, picture,cate_gory,title,price}  = category || {}
+    return (
+        <div>
+            <div className="relative flex w-full max-w-[48rem] flex-row rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+  <div className="relative m-0 w-2/5 shrink-0 overflow-hidden rounded-xl rounded-r-none bg-white bg-clip-border text-gray-700">
+    <img
+      src={picture}
+      alt="image"
+      className="h-full w-full object-cover"
+    />
+  </div>
+  <div className="p-6">
+    <h6 className="mb-4 block font-sans text-base font-semibold uppercase leading-relaxed tracking-normal text-pink-500 antialiased">
+       {cate_gory}
+    </h6>
+    <h4 className="mb-2 block font-sans text-2xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+       {title}
+    </h4>
+    <p className="mb-8 block font-sans text-base font-normal leading-relaxed text-gray-700 antialiased">
+      $ {price}
+    </p>
+    <a className="inline-block" href="#">
+      <Link to={`/categories/${id}`}>
+        <button
+        className="flex  items-center gap-2 rounded-lg py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-pink-500 transition-all hover:bg-pink-500/10 active:bg-pink-500/30 "
+        type="button"
+      >
+        view Details
+        
+      </button>
+                        </Link>
+    </a>
+  </div>
+</div>
+        </div>
+    );
+};
+
+DonationCard.propTypes = {
+    category:PropTypes.object
+};
+
+export default DonationCard;
