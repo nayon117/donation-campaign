7a0a54a0088e3afd94a23f5dbd79d9679fad5ef3
nayon117 <hasibulhasannayon260@gmail.com>
2023-09-25 14:53:16 +0600

    set details item to localStorage and implement sweet alert

--- src/pages/Category/CategoryCard.jsx
+++ src/pages/Category/CategoryCard.jsx
@@ -1,13 +1,38 @@
  
 import PropTypes from 'prop-types';
+import swal from 'sweetalert';
 
 const CategoryCard = ({ category }) => {
-     const {picture,title,description} = category || {}
+    const { picture, title, description, price,text_color } = category || {}
+    const textColor = {
+        backgroundColor: text_color,
+        color:"white"
+    }
+    // onclick funtionality
+   
+    const handleDonate = () => {
+        const donationsArray = []
+        const donationsItems = JSON.parse(localStorage.getItem("donations"))
+        if (!donationsItems) {
+            donationsArray.push(category)
+            localStorage.setItem('donations', JSON.stringify(donationsArray))
+            swal("Thanks!", "Donate Succeed!", "success");
+        }
+        else {
+            donationsArray.push(...donationsItems, category)
+            localStorage.setItem('donations', JSON.stringify(donationsArray))
+            swal("Thanks!", "Donate Succeed!", "success");
+        }
+    }
+    
     return (
-        <div className='min-h-screen flex items-center justify-center'>
-            <div>
-            <img style={{maxWidth:'100%'}} className='w-full h-[550px] mx-auto object-cover   ' src={picture} alt="" />
-            <h2>{ title}</h2>
+        <div className='min-h-screen flex items-center justify-center py-10  '>
+            <div className='space-y-4 relative  '>
+                <img style={{ maxWidth: '100%' }} className='w-full h-[550px] mx-auto object-cover rounded-md   ' src={picture} alt="" />
+                <div className='hero-overlay bg-opacity-50 absolute top-2/3   w-full h-20  '>
+                <button onClick={handleDonate} className='btn ml-5 mt-4' style={textColor}>Donate $ {price }</button>
+               </div>
+            <h2 className='text-3xl font-bold py-2 '>{ title}</h2>
             <p>{ description}</p>
            </div>
         </div>
